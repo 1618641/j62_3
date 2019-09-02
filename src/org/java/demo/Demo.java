51d5f08efb183c66bb82d53f13f382ca037f1b72
1618641 <1618641@qq.com>
2019-09-02 08:32:18 +0800

    www

---
+++ src/org/java/demo/Demo.java
@@ -0,0 +1,12 @@
+package org.java.demo;
+
+/**
+ * @Author: 马果
+ * @Date: 2019/9/2 08:30
+ * @Description:
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("@@@@@@@@@@@@@@");
+    }
+}
